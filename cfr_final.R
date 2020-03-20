231982a2ec6f7fff2084aa88fad1aaa125d5aa42
Ivan Korolev <ivankorolev89@gmail.com>
2020-03-20 09:57:55 -0400

    Update cfr_final.R

--- cfr_final.R
+++ cfr_final.R
@@ -8,8 +8,8 @@ p2 <- .8 # survival probability for bad health
 q1 <- 0.1 # probability of getting the virus for good health
 q2 <- 0.2 # probability of getting the virus for bad health
 
-CFR_V <- (a*q1*(1-p1)+(1-a)*q2*(1-p2))/(a*q1+(1-a)*q2) # P(die|virus)
-CFR_noV <- (a*(1-q1)*(1-p1)+(1-a)*(1-q2)*(1-p2))/(a*(1-q1)+(1-a)*(1-q2)) # P(die| no virus)
+CFR_V <- (a*q1*(1-p1)+(1-a)*q2*(1-p2))/(a*q1+(1-a)*q2) # P(die | virus)
+CFR_noV <- (a*(1-q1)*(1-p1)+(1-a)*(1-q2)*(1-p2))/(a*(1-q1)+(1-a)*(1-q2)) # P(die | no virus)
 
 # Results 
 CFR_V*100
